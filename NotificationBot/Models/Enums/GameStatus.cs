b9030320480cf5273efdac8603a65cf8b79f98ff
Simon Zhang <simonzhang843@gmail.com>
2023-04-27 19:02:30 -0400

    Add status description

--- NotificationBot/Models/Enums/GameStatus.cs
+++ NotificationBot/Models/Enums/GameStatus.cs
@@ -1,11 +1,19 @@
-﻿namespace NotificationBot.Models.Enums
+﻿
+using System.ComponentModel;
+
+namespace NotificationBot.Models.Enums
 {
     public enum GameStatus
     {
+        [Description("None")]
         None = 0,
+        [Description("Away")]
         Away = 1,
+        [Description("Pending")]
         Pending = 2,
+        [Description("Finished")]
         Finished = 3,
+        [Description("Invalid")]
         Invalid = 4,
     }
 }
